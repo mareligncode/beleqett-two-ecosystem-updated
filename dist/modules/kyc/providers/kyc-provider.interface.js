9fc88a96ab06b9e0525938c4f1612f0b15d1bd8f
natnael <ketemanatnael023@gmail.com>
2026-07-05 01:38:35 +0300

    untrack dist folder

--- dist/modules/kyc/providers/kyc-provider.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=kyc-provider.interface.js.map
--- dist/modules/kyc/providers/mock-kyc-provider.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { KycProvider, KycVerificationResult } from './kyc-provider.interface';
-export declare class MockKycProvider implements KycProvider {
-    private readonly logger;
-    verify(documentBuffer: Buffer, faceScanBuffer: Buffer): Promise<KycVerificationResult>;
-}
